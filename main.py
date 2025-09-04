8987a43e2c8dc7c99e6ac4f1e5faaf290416284b
Ghosh <Aniruddha.Ghosh@medstar.net>
2025-09-04 01:14:14 -0500

    removed ChatResponse

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from ollama import chat
-from ollama import ChatResponse
 
 response = chat(
                 model='llama2', 
                 messages=[{'role': 'user',
-                            'content': 'explain in a sentance what is quantum mechanics?',
+                            'content': 'explain in a sentance what is quantum mechanics?'
                         },]
                 )
 
